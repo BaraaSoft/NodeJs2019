811ef0e83538625871863f67167bfdddc59c9f76
baraa <baraaabuzeid@hotmail.com>
2019-08-08 01:21:24 +0800

    basic nodeJs sets

--- SecProj/app.js
+++
@@ -1,9 +0,0 @@
-import { ListDir, ListDirPromise } from './ListDir'
-
-
-const listDir = ListDirPromise('./')
-listDir.Directories()
-listDir.on('onFileLog', console.log)
-console.log("ddd")
-
-
--- SecProj/src/app.js
+++ SecProj/src/app.js
@@ -1,1 +1,20 @@
-import express from 'express'
+import http from 'http'
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+
+import config from './config';
+import routes from './routes';
+
+const app = express();
+
+app.server = http.createServer(app);
+
+app.use('/v1', routes);
+app.server.listen(config.port)
+
+console.log("starting on port " + app.server.address().port)
+
+
+export default app
